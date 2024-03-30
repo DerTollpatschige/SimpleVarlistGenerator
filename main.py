6a70667e1c872fd59f364fb4aedd451ae00c0ffa
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-03-30 13:54:02 +0100

    Update main.py
    - Fixed a problem where variables were inexplicably left out.

--- main.py
+++ main.py
@@ -4,24 +4,17 @@ import os
 names = set()
 scriptFiles = []
 
-def collectVariableNames(line, names):
-    loadPattern = r"\d+\s+\(L\.L\.(.*?)\)"
-    storePattern = r"\d+\s+\(S\.L\.(.*?)\)"
 
-    loadMatch = re.search(loadPattern, line)
-    storeMatch = re.search(storePattern, line)
-
-    if loadMatch:
-        varname = loadMatch.group(1)
-    elif storeMatch:
-        varname = storeMatch.group(1)
-    else:
-        return None
+def findVariableNames(file):
+    with open(file, 'r') as f:
+        contents = f.read()
     
-    if varname and varname not in names:
-        names.add(varname)
-        return varname
-    return None
+    pattern = r"\((S\.L|L\.L)\.(\w+)\)"
+    matches = re.findall(pattern, contents)
+    variables = [match[1] for match in matches]
+
+    return variables
+
 
 def findScriptFiles(filePath):
     for root, dirs, files in os.walk(filePath):
@@ -35,12 +28,8 @@ def modifyFileName(fileName):
 def processScriptFile(file, output):
     names.clear()
     try:
-        with open(file, 'r', encoding='cp1252') as file:
-            for line in file:
-                varname = collectVariableNames(line, names)
-                if varname:
-                    names.add(varname)
-
+        for variable in findVariableNames(file):
+            names.add(variable)
         with open(output, 'w') as varlist:
             for name in names:
                 varlist.write(f"{name}\n")
